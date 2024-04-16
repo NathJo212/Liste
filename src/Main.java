ce8f79609209b51bf67e1f52af0ad3639e062d19
Nathan J <nathanjobin@outlook.com>
2024-04-16 08:50:06 -0400

    Retrait du test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Allo");
         System.out.println("\nTest 1: Création d'une liste vide");
         Liste data = new Liste();
         System.out.println(data);
